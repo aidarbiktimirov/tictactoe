58880a3c2252e767418d1e604204257d1c8903ac
Aydar Biktimirov <aydar@biktimirov.net>
2015-11-03 16:42:28 +0300

    Added methods for static files serving

--- tictactoe_server/main.go
+++ tictactoe_server/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
     "github.com/aidarbiktimirov/tictactoe/api"
+
+    "io/ioutil"
+    "net/http"
+    "fmt"
+    "os"
 )
+
+func createStaticHandler(filename, mimetype string) func (w http.ResponseWriter, r *http.Request) {
+    return func (w http.ResponseWriter, r *http.Request) {
+    data, _ := ioutil.ReadFile(fmt.Sprintf("%s/%s", os.Args[1], filename))
+        w.Header().Set("Content-Type", fmt.Sprintf("%s; charset=utf-8", mimetype))
+        fmt.Fprintf(w, "%s", data)
+    }
+}
 
 func main() {
     srv := api.NewServer()
-    // srv.HandleFunc("/tictactoe/", handleWebPage)
-    // srv.HandleFunc("/static/", handleStaticFiles)
+    srv.HandleFunc("/out.js", createStaticHandler("out.js", "application/javascript"))
+    srv.HandleFunc("/", createStaticHandler("index.html", "text/html"))
     srv.Run(":10000")
 }
